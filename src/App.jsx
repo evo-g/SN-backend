c7d0a2766b05dfbd49137e7ee6fe8fcaa4737c45
Evodio Garcia <evodiog@gmail.com>
2023-06-16 14:30:18 -0700

    adding headers into fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import './App.css'
 
 
 const getKBSNData = async () => {
-  const response = await fetch('https://dev103286.service-now.com/api/495743/react_test');
+  const url = 'https://dev103286.service-now.com/api/495743/react_test';
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
   const data = await response.json();
   // console.log(data);
   return data.result;
