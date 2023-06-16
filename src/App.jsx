a3725bcc441cb14406f583c41a5739a421cc85b1
Evodio Garcia <evodiog@gmail.com>
2023-06-16 14:32:34 -0700

    adding headers into fetch change to cors

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const getKBSNData = async () => {
   const url = 'https://dev103286.service-now.com/api/495743/react_test';
   const response = await fetch(url, {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
-    mode: "no-cors", // no-cors, *cors, same-origin
+    mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
     headers: {
